581b159b9c1ce81aea825f63ac44bad095f366f5
jonchao <jon.chao33@gmail.com>
2021-03-13 23:37:23 -0800

    Hero and Email

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,11 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Jon Chao.</h2>;
+  const three = <h3 className="big-heading">I design things for the web.</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a UX/UI designer based in Oakland, CA specializing in designing exceptional websites, applications, and everything in between.
     </p>
   );
   const five = (
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'jon.chao33@gmail.com',
 
   socialMedia: [
     {
