b9e29d9952775a6b1d10008a077bfc330c4d793c
jonchao <jon.chao33@gmail.com>
2021-09-30 21:50:09 -0700

    Revert "Update loader.js"
    This reverts commit 0835d90959650fe542ac05fd2be6e4171e4bdf0a.

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -12,7 +12,7 @@ const IconLoader = () => (
       </g>
       <path
         stroke="currentColor"
-        strokeWidth="100"
+        strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M 50, 5
