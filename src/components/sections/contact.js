5b6dfa69a5050aa8d463ae83ae3ec26bef8e7168
jonchao <jon.chao33@gmail.com>
2021-09-30 20:53:31 -0700

    Updated Get In Touch Section

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -48,15 +48,14 @@ const Contact = () => {
     <StyledContactSection id="contact" ref={revealContainer}>
       <h2 className="numbered-heading overline">What’s Next?</h2>
 
-      <h2 className="title">Get In Touch</h2>
+      <h2 className="title">Let's Chat!</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        Feel free to reach out anytime. Whether you just want to say hi or have a question, I'll try my best to get back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        Say Hi!
       </a>
     </StyledContactSection>
   );
